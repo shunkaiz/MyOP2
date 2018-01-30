52f057448802b70dac282dd1d3c03f863225e254
shunkaiz <shunkaiz@usc.com>
2018-01-29 22:46:28 -0800

    layouts

--- op.js
+++ op.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var exphbs = require('express-handlebars');
 
-
+var index = require('./routes/index.js');
+var users = require('./routes/users.js');
 
 
 
@@ -12,13 +13,16 @@ var app = express();
 app.set('views',  path.join(__dirname, 'views'));
 
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-
+app.set('view engine', 'handlebars');
 //set up static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//set up the port
 app.set('port', (process.env.PORT || 3000));
-
 app.listen(app.get('port'), function(){
 	console.log('port'+ app.get('port')+'is established');
 });
+
+
+app.use('/', index);
+app.use('/users', users);
