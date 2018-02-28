b5a4a27008e3590a7c45fca599df728cb558c9ea
shunkaiz <shunkaiz@usc.com>
2018-02-28 00:06:29 -0800

    fix bg slider bugs

--- public/js/bg-slider.js
+++ public/js/bg-slider.js
@@ -1,16 +1,16 @@
 $(function(){
 	var curr_page = 1;
-	var page = 4;
+	var page_num = 4;
 	$("#next").click(function(){
 		$parent = $(this).parents("div.body-bg");
 		$list = $parent.find("div.bg-list");
-		$width = $parent.width();
+		$width = 1903;
 		if( !$list.is(":animated") ){
-			if(page<=curr_page){
+			if(curr_page >= page_num){
 				$list.animate({ left : '0px'}, "slow");
-				curr_page  =1;
+				curr_page = 1;
 			}else{
-				$list.animate({ left : '-='+"1903px" }, "slow");
+				$list.animate({ left : '-='+ $width }, "slow");
 				$list.animate({	left : "-=" +"4px"}, 1);
 				curr_page++
 			}
@@ -19,12 +19,13 @@ $(function(){
 	$("#prev").click(function(){
 		$parent = $(this).parents("div.body-bg");
 		$list = $parent.find("div.bg-list");
-		$width = $parent.width();
+		$width = 1903;
 		if( !$list.is(":animated") ){
-			if(1==curr_page){
-				$list.animate({ left : '0px'}, "slow");
+			if(curr_page == 1){
+				$list.animate({ left : '-5709px'}, "slow");
+				curr_page = 4;
 			}else{
-				$list.animate({ left : '+='+$width }, "slow");
+				$list.animate({ left : '+='+ $width }, "slow");
 				$list.animate({	left : "+=" +"4px"}, 1);
 				curr_page--
 			}
