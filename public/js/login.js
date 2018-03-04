3a55aa9a9fecae3c6e12f3c37b8e676e14a8fea6
shunkaiz <shunkaiz@usc.edu>
2018-03-03 16:12:20 -0800

    fix layout bugs on login and signup page

--- public/js/login.js
+++ public/js/login.js
@@ -1,38 +1,71 @@
 $(function(){
-	   		$("#email2").blur(function(event){
-				$value = $(this).val();
-				$(this).popover('hide');
-				$($(this).parent()).find(".glyphicon").remove();
-	   			$(this).popover('hide');
-	   			if($(this).val() == ""){
-	   				$($(this).parent()).append("<span class='glyphicon glyphicon-ban-circle customempty'></span>");
-	   			}else if(!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test($value ))){
-	   				$($(this).parent()).append("<span class='glyphicon glyphicon-remove-circle customfalse'></span>");
-	   			}else{
-	   				$($(this).parent()).append("<span class='glyphicon glyphicon-ok-circle customtrue'></span>");
-	   				$(this).popover('hide');
-	   			}	   			
-	   		}).keyup(function(){
-	   			$(this).triggerHandler("blur");
-	   		}).focus(function(){
-	   			$(this).popover('show');
-	   		});	
-	   		$("#password2").blur(function(){	   			
-	   			$($(this).parent()).find(".glyphicon").remove();
-	   			$(this).popover('hide');
-	   			if($(this).val() == ""){
-	   				$($(this).parent()).append("<span class='glyphicon glyphicon-ban-circle customempty'></span>");
-	   			}
-	   			else{
-	   				$($(this).parent()).append("<span class='glyphicon glyphicon-ok-circle customtrue'></span");
-	   			}	   			
-	   		}).keyup(function(){
-	   			$(this).triggerHandler("blur");
-	   		}).focus(function(){
-	   			$(this).popover('show');
-	   		});
+	$("#loginUsername").blur(function(event){
+	$(this).popover('hide');
+		$(this).popover('hide');
+		if($(this).val() == ""){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ban-circle customempty');
+		}else if($(this).val().length<6){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-remove-circle customfalse');
+		}else{
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ok-circle customtrue');
+		}	   			
+	}).keyup(function(){
+		$(this).triggerHandler("blur");
+	}).focus(function(){
+		$(this).popover('show');
+	});	
 
-	$("#submit2").click(function(){
+
+	$("#loginEmail").blur(function(){	   			
+		$email = $(this).val();
+		$(this).popover('hide');
+		if($(this).val() === ""){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ban-circle customempty');
+		}else if(!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test($email ))){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-remove-circle customfalse');
+		}else{
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ok-circle customtrue');
+		}	   			
+	}).keyup(function(){
+		$(this).triggerHandler("blur");
+	}).focus(function(){
+		$(this).popover('show');
+	});
+
+
+	$("#loginPassword").blur(function(){	   			
+		$(this).popover('hide');
+		if($(this).val() == ""){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ban-circle customempty');
+		}
+		else{
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ok-circle customtrue');
+		}	   			
+	}).keyup(function(){
+		$(this).triggerHandler("blur");
+	}).focus(function(){
+		$(this).popover('show');
+	});	
+
+
+	$("#loginRePassword").blur(function(){	   			
+		$psw = $("#loginPassword").val();
+		$(this).popover('hide');
+		if($(this).val() == ""){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ban-circle customempty');
+		}else if($psw === $(this).val()){
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-ok-circle customtrue');
+		}else{
+			$($(this).parent()).find(".glyIcon").attr('class', 'glyIcon glyphicon glyphicon-remove-circle customfalse');
+		}	   			
+	}).keyup(function(){
+		$(this).triggerHandler("blur");
+	}).focus(function(){
+		$(this).popover('show');
+	});
+
+
+	$("#loginSubmit").click(function(){
 		event.preventDefault();	
 		if(!$("#email2").val() || !$("#password2").val()){
 			alert("Username or Password cannot be empty!");
