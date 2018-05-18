dea502ff21dcd45c1c4709d96a4db58ae20c85f9
shunkaiz <shunkaiz@usc.edu>
2018-05-18 21:52:08 +0800

    add intro video

---
+++ public/js/introVideo.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".bg-list").css({'display':'none'});
+    setTimeout(()=>{
+        $(".intro-video").css({'display': 'none'});
+    }, 6000);
+    setTimeout(()=>{
+        $(".bg-list").css({'display':'block'});
+    }, 6000);
+});
